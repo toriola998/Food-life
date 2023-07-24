e8b4f65aca963a16e1e3cd8b22f4a63c1110f9ac
Toriola <faaedah@gmail.com>
2023-07-24 12:06:48 +0100

    ADD: added functionalities to modals

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const home = document.querySelector('.home');
 const viewBtn = document.querySelector('.view')
 const completed = document.querySelector('.profile-completed');
-console.log(home, viewBtn)
 
-viewBtn.addEventListener("click", function(){
-    home.style.display = "none";
-    completed.style.display = "block"
-})
+if(home && completed) {
+    viewBtn.addEventListener("click", () => {
+        home.style.display = "none";
+        completed.style.display = "block"
+    })
+}
+
+const viewFoodBtn = document.querySelector('.view-food');
+const viewCleaningBtn = document.querySelector('.view-cleaning')
+const foodModal = document.querySelector('.food-overlay')
+const cleaningModal = document.querySelector('.cleaning-overlay')
+
+//SHOW MODALS WHEN VIEW STATUS IS CLICKED
+if(viewFoodBtn) {
+    viewFoodBtn.addEventListener('click', () => {
+        foodModal.style.display = "flex"
+    })
+}
+
+if(viewCleaningBtn) {
+    viewCleaningBtn.addEventListener('click', () => {
+        cleaningModal.style.display = "flex"
+    })
+}
+
+//CLOSE MODALS WITH CLOSE ICON
+const closeFoodBtn = document.querySelector('.close-food-btn')
+const closeCleaningBtn = document.querySelector('.close-cleaning-btn')
+
+if(closeFoodBtn) {
+    closeFoodBtn.addEventListener('click', () => {
+        foodModal.style.display = "none"
+    })
+}
+
+if(closeCleaningBtn) {
+    closeCleaningBtn.addEventListener('click', () => {
+        cleaningModal.style.display = "none"
+    })
+}
